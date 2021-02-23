ed87a5065740a91095de5b3706c361ebe7a61cd0
Evgenia <zhenja_a@bk.ru>
2021-02-23 13:08:05 +0300

    Готовый клиент-сервер чат

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,65 @@
-public class Client {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class Client {
+    private static final String SERVER_ADDR = "localhost";
+    private static final int SERVER_PORT = 8189;
+
+    public static void main(String[] args) {
+        System.out.println("Client started");
+        try(Socket socket = new Socket(SERVER_ADDR, SERVER_PORT)){
+                System.out.println("Client connected");
+                Scanner socket_in = new Scanner(socket.getInputStream());
+                Scanner console_in = new Scanner(System.in);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            while(true){
+                                String fromServer = socket_in.nextLine();
+                                if (fromServer.equals("/end")){
+                                    System.out.println("Server disconnected");
+                                    break;
+                                }
+                                System.out.println("Server: " + fromServer);
+                            }
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+                while (true){
+                    System.out.println("Введите сообщение для сервера");
+                    String s = console_in.nextLine();
+                    if (s.equals("/end")){
+                        System.out.println("Client disconnected");
+                        out.println(s);
+                        break;
+                    }
+                    out.println(s);
+                }
+               new Thread(new Runnable() {
+                   @Override
+                   public void run() {
+                       try {
+                           while(true){
+                               String fromServer = socket_in.nextLine();
+                               if (fromServer.equals("/end")){
+                                   System.out.println("Server disconnected");
+                                   break;
+                               }
+                               System.out.println("Server: " + fromServer);
+                           }
+                       }catch (Exception e){
+                           e.printStackTrace();
+                       }
+                   }
+               }).start();
+        }catch (IOException e) {
+                e.printStackTrace();
+        }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,45 @@
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class Server {
+    private static final int PORT = 8189;
+    public static void main(String[] args) {
+        try(ServerSocket server = new ServerSocket(PORT)){
+            System.out.println("Server started");
+            try(Socket socket = server.accept()) {
+                System.out.println("Client connected");
+                Scanner scaner = new Scanner(socket.getInputStream());
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Scanner console_in = new Scanner(System.in);
+                        while (true) {
+                            System.out.println("Введите сообщение для клиента");
+                            String fromConsole = console_in.nextLine();
+                            if (fromConsole.equals("/end")) {
+                                System.out.println("Server disconnected");
+                                break;
+                            }
+                            out.println(fromConsole);
+                        }
+                    }
+                }).start();
+                while (true) {
+                    String s = scaner.nextLine();
+                    if (s.equals("/end")) {
+                        System.out.println("Client disconnected");
+                        break;
+                    }
+                    System.out.println("Client: " + s);
+                }
+
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
